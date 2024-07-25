7cb76e910d0dbc875b079ae5032477865004a26c
williambza <william.brander@gmail.com>
2024-07-25 11:38:07 +0200

    Add client

---
+++ src/ClientUI/Program.cs
@@ -0,0 +1,60 @@
+﻿Console.Title = "Load (ClientUI)";
+Console.SetWindowSize(65, 15);
+
+var endpointConfiguration = new EndpointConfiguration("ClientUI");
+var transport = endpointConfiguration.UseTransport<LearningTransport>();
+
+endpointConfiguration.AuditProcessedMessagesTo("audit");
+endpointConfiguration.SendHeartbeatTo("Particular.ServiceControl");
+
+endpointConfiguration.UniquelyIdentifyRunningInstance()
+    .UsingCustomIdentifier(new Guid("EA3E7D1B-8171-4098-B160-1FEA975CCB2C"))
+    .UsingCustomDisplayName("original-instance");
+
+var metrics = endpointConfiguration.EnableMetrics();
+metrics.SendMetricDataToServiceControl(
+    "Particular.Monitoring",
+    TimeSpan.FromMilliseconds(500)
+);
+
+var routing = transport.Routing();
+routing.RouteToEndpoint(typeof(PlaceOrder), "Sales");
+
+var endpointInstance = await Endpoint.Start(endpointConfiguration);
+
+var simulatedCustomers = new SimulatedCustomers(endpointInstance);
+var cancellation = new CancellationTokenSource();
+var simulatedWork = simulatedCustomers.Run(cancellation.Token);
+
+RunUserInterfaceLoop(simulatedCustomers);
+
+cancellation.Cancel();
+
+await simulatedWork;
+
+await endpointInstance.Stop();
+
+void RunUserInterfaceLoop(SimulatedCustomers simulatedCustomers)
+{
+    while (true)
+    {
+        Console.Clear();
+        Console.WriteLine("Simulating customers placing orders on a website");
+        Console.WriteLine("Press T to toggle High/Low traffic mode");
+        Console.WriteLine("Press ESC to quit");
+        Console.WriteLine();
+
+        simulatedCustomers.WriteState(Console.Out);
+
+        var input = Console.ReadKey(true);
+
+        switch (input.Key)
+        {
+            case ConsoleKey.T:
+                simulatedCustomers.ToggleTrafficMode();
+                break;
+            case ConsoleKey.Escape:
+                return;
+        }
+    }
+}
---
+++ src/ClientUI/SimulatedCustomers.cs
@@ -0,0 +1,69 @@
+﻿using Messages;
+
+namespace ClientUI;
+
+class SimulatedCustomers(IEndpointInstance endpointInstance)
+{
+    public void WriteState(TextWriter output)
+    {
+        var trafficMode = highTrafficMode ? "High" : "Low";
+        output.WriteLine($"{trafficMode} traffic mode - sending {rate} orders / second");
+    }
+
+    public void ToggleTrafficMode()
+    {
+        highTrafficMode = !highTrafficMode;
+        rate = highTrafficMode ? 8 : 1;
+    }
+
+    public async Task Run(CancellationToken cancellationToken = default)
+    {
+        nextReset = DateTime.UtcNow.AddSeconds(1);
+        currentIntervalCount = 0;
+
+        while (!cancellationToken.IsCancellationRequested)
+        {
+            var now = DateTime.UtcNow;
+            if (now > nextReset)
+            {
+                currentIntervalCount = 0;
+                nextReset = now.AddSeconds(1);
+            }
+
+            await PlaceSingleOrder(cancellationToken);
+            currentIntervalCount++;
+
+            try
+            {
+                if (currentIntervalCount >= rate)
+                {
+                    var delay = nextReset - DateTime.UtcNow;
+                    if (delay > TimeSpan.Zero)
+                    {
+                        await Task.Delay(delay, cancellationToken);
+                    }
+                }
+            }
+            catch (TaskCanceledException)
+            {
+                break;
+            }
+        }
+    }
+
+    Task PlaceSingleOrder(CancellationToken cancellationToken)
+    {
+        var placeOrderCommand = new PlaceOrder
+        {
+            OrderId = Guid.NewGuid().ToString()
+        };
+
+        return endpointInstance.Send(placeOrderCommand, cancellationToken);
+    }
+
+    bool highTrafficMode;
+
+    DateTime nextReset;
+    int currentIntervalCount;
+    int rate = 1;
+}
