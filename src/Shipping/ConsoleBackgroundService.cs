7cb4a2e9f3dabddce29a88b847a98cb0f50098a2
Ramon Smits <ramon.smits@gmail.com>
2024-10-02 15:37:21 +0200

    namespace not using scope

--- src/Shipping/ConsoleBackgroundService.cs
+++ src/Shipping/ConsoleBackgroundService.cs
@@ -1,56 +1,55 @@
-﻿namespace Shipping
+﻿namespace Shipping;
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Extensions.Hosting;
+
+class ConsoleBackgroundService(SimulationEffects state) : BackgroundService
 {
-    using System.Threading;
-    using System.Threading.Tasks;
-    using Microsoft.Extensions.Hosting;
-
-    class ConsoleBackgroundService(SimulationEffects state) : BackgroundService
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
     {
-        protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+        while (true)
         {
-            while (true)
+            Console.Clear();
+            await Console.Out.WriteAsync("""
+                Shipping Endpoint:
+
+                - Press I to increase the simulated failure rate
+                - Press D to decrease the simulated failure rate
+                - Press R to reset simulation
+                - Press CTRL+C to quit
+
+                """);
+
+            state.WriteState(Console.Out);
+
+            while (!Console.KeyAvailable)
             {
-                Console.Clear();
-                await Console.Out.WriteAsync("""
-                    Shipping Endpoint:
+                await Task.Delay(15, stoppingToken);
+            }
 
-                    - Press I to increase the simulated failure rate
-                    - Press D to decrease the simulated failure rate
-                    - Press R to reset simulation
-                    - Press CTRL+C to quit
+            var input = Console.ReadKey(true);
 
-                    """);
-
-                state.WriteState(Console.Out);
-
-                while (!Console.KeyAvailable)
-                {
-                    await Task.Delay(15, stoppingToken);
-                }
-
-                var input = Console.ReadKey(true);
-
-                switch (input.Key)
-                {
-                    case ConsoleKey.I:
-                        state.IncreaseFailureRate();
-                        break;
-                    case ConsoleKey.D:
-                        state.DecreaseFailureRate();
-                        break;
-                    case ConsoleKey.W:
-                        state.ToggleDegradationSimulation();
-                        break;
-                    case ConsoleKey.F:
-                        state.ProcessMessagesFaster();
-                        break;
-                    case ConsoleKey.S:
-                        state.ProcessMessagesSlower();
-                        break;
-                    case ConsoleKey.R:
-                        state.Reset();
-                        break;
-                }
+            switch (input.Key)
+            {
+                case ConsoleKey.I:
+                    state.IncreaseFailureRate();
+                    break;
+                case ConsoleKey.D:
+                    state.DecreaseFailureRate();
+                    break;
+                case ConsoleKey.W:
+                    state.ToggleDegradationSimulation();
+                    break;
+                case ConsoleKey.F:
+                    state.ProcessMessagesFaster();
+                    break;
+                case ConsoleKey.S:
+                    state.ProcessMessagesSlower();
+                    break;
+                case ConsoleKey.R:
+                    state.Reset();
+                    break;
             }
         }
     }
