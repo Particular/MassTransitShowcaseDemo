8757406971175276299bb23fa5ee4ad46b60d491
John Simons <john.simons@particular.net>
2025-01-21 11:58:01 +1000

    Use external queues.txt file
    use latest images for platform

--- src/Helper/BusRegistrationConfiguratorExt.cs
+++ src/Helper/BusRegistrationConfiguratorExt.cs
@@ -39,7 +39,7 @@ public static class BusRegistrationConfiguratorExt
         {
             x.UsingRabbitMq((context, cfg) =>
             {
-                cfg.Host("localhost", 56721, "/", h =>
+                cfg.Host("localhost", 33721, "/", h =>
                 {
                     h.Username("guest");
                     h.Password("guest");
